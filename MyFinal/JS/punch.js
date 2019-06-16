3cd8fb246f5caf339a04337aa5c2a94e963ced65
Hsieh <hchs910569.hchs@gmail.com>
2019-06-16 18:25:59 +0800

    UP

--- MyFinal/JS/punch.js
+++ MyFinal/JS/punch.js
@@ -9,10 +9,10 @@ console.log("Test javascript is ok")
 // 設定韓國瑜頭的位置
 // 上面圖層
 var d1_LOC = [
-    [240, 410],
-    [435, 440],
-    [625, 400],
-    [785, 460]
+    [380, 410],
+    [575, 425],
+    [760, 400],
+    [920, 460]
 ]
 
 for (let i = 1; i < d1_LOC.length + 1; i++) {
@@ -23,10 +23,10 @@ for (let i = 1; i < d1_LOC.length + 1; i++) {
 
 // 下面圖層
 var d0_LOC = [
-    [50, 440],
-    [230, 500],
-    [520, 530],
-    [800, 550]
+    [190, 440],
+    [370, 500],
+    [650, 530],
+    [935, 550]
 ]
 
 for (let i = 1; i < d0_LOC.length + 1; i++) {
@@ -57,8 +57,16 @@ var Showhead = (head, show = 0) => {
     }, 1000, function() {
         $(head).hide();
         $(head).css("top", origin);
-        $(head).attr("src", "IMG/Head1.png");
-        $(head).css("width", "160px");
+
+        if (getRandom(0, 1) == 1) {
+            $(head).attr("src", "IMG/Head1.png");
+            $(head).css("width", "160px");
+        } else {
+            $(head).attr("src", "IMG/K.png");
+            $(head).css("width", "170px");
+        }
+
+
         //$(head).finish();
         setTimeout(function() {
             Showhead(head, 1)
@@ -85,14 +93,23 @@ Showhead('#d0 > #Hanhead_4', 0)
 
 // 打到韓國瑜的頭換頭
 $("#Hanhead_1, #Hanhead_2, #Hanhead_3,#Hanhead_4").on("click", function(event) {
-    console.log(event.currentTarget)
-    $(event.currentTarget).attr("src", "IMG/Hanhead2.png")
-    $(event.currentTarget).css("width", "200px")
+    console.log($(event.currentTarget).attr("src"))
+
+    if ($(event.currentTarget).attr("src") == "IMG/Head1.png") {
+        $(event.currentTarget).attr("src", "IMG/Hanhead2.png")
+        $(event.currentTarget).css("width", "200px")
+    }
+    if ($(event.currentTarget).attr("src") == "IMG/K.png") {
+        $(event.currentTarget).attr("src", "IMG/K2.png")
+        $(event.currentTarget).css("width", "170px")
+
+    }
+
 })
 
 // 定義滑鼠圖示
 $(function() {
-    $("#testarea").on("click", function() {
+    $("#testarea,#Show").on("click", function() {
         $("#MouseIMG").attr("src", "IMG/knocked.png")
             // wait(2)
 
@@ -106,21 +123,21 @@ $(function() {
         // console.log("click")
     })
 
-    $("#testarea").mousemove(function(e) {
+    $("#testarea,#Show").mousemove(function(e) {
         $(".cursor").show().css({
             "left": e.clientX - 100,
-            "top": e.clientY - 50
+            "top": e.clientY - 100
         });
     })
 
-    $(".Hanhead").mousemove(function(e) {
+    $(".Hanhead,#Show").mousemove(function(e) {
         $(".cursor").show().css({
             "left": e.clientX - 100,
-            "top": e.clientY - 50
+            "top": e.clientY - 100
         });
     })
 
-    $(".Hanhead").on("click", function() {
+    $(".Hanhead,#Show").on("click", function() {
         $("#MouseIMG").attr("src", "IMG/knocked.png")
             // wait(2)
 
@@ -134,3 +151,32 @@ $(function() {
         // console.log("click")
     })
 })
+
+
+// 倒數計時器
+$("#time > p").text("10")
+
+
+var time = +$("#time > p").text()
+var t = time;
+
+function showTime() {
+
+    t -= 1;
+    time -= 1
+
+    $("#time > p").text(time)
+    if (t < 10) {
+        $("#time > p").text("0" + time)
+    }
+
+    if (t == 0) {
+        //location.href = 'http://www.dotblogs.com.tw/puma/';
+        return 0;
+    }
+
+    //每秒執行一次,showTime()
+    setTimeout("showTime()", 1000);
+}
+
+showTime()
